6fd70458331e27c49a0410d791aedc087f2d09a9
Rutvi Rajesh <rutvi.rajesh@simformsolutions.com>
2026-09-02 11:24:58 +0530

    chore: migrate notification-service/src/main/java/com/piggymetrics/notification/NotificationServiceApplication.java to aws

--- notification-service/src/main/java/com/piggymetrics/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/piggymetrics/notification/NotificationServiceApplication.java
@@ -4,7 +4,7 @@ import com.piggymetrics.notification.repository.converter.FrequencyReaderConvert
 import com.piggymetrics.notification.repository.converter.FrequencyWriterConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import java.util.Arrays;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableOAuth2Client
 @EnableFeignClients
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -34,6 +33,37 @@ public class NotificationServiceApplication {
 		public CustomConversions customConversions() {
 			return new CustomConversions(Arrays.asList(new FrequencyReaderConverter(),
 					new FrequencyWriterConverter()));
+		}
+	}
+
+	@Configuration
+	static class ServiceUrlConfiguration {
+
+		@Bean
+		@ConfigurationProperties(prefix = "service.urls")
+		public ServiceUrls serviceUrls() {
+			return new ServiceUrls();
+		}
+	}
+
+	public static class ServiceUrls {
+		private String accountService;
+		private String statisticsService;
+
+		public String getAccountService() {
+			return accountService;
+		}
+
+		public void setAccountService(String accountService) {
+			this.accountService = accountService;
+		}
+
+		public String getStatisticsService() {
+			return statisticsService;
+		}
+
+		public void setStatisticsService(String statisticsService) {
+			this.statisticsService = statisticsService;
 		}
 	}
 }
