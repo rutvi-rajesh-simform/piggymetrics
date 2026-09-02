a5226787c98cdec0dced44b9239e69f56888a0ec
Rutvi Rajesh <rutvi.rajesh@simformsolutions.com>
2026-09-02 11:24:52 +0530

    chore: migrate account-service/src/main/java/com/piggymetrics/account/AccountApplication.java to aws

--- account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
+++ account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
@@ -2,17 +2,22 @@ package com.piggymetrics.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
+/**
+ * Account service bootstrap.
+ *
+ * Service-to-service endpoints should be supplied through externalized configuration
+ * (environment/application properties) to support cloud-native service URL resolution.
+ *
+ * Shared dependency management for AWS SDK v2 and Spring Cloud AWS Secrets Manager
+ * integration is handled in the build configuration layer.
+ */
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableOAuth2Client
 @EnableFeignClients
-@EnableCircuitBreaker
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AccountApplication {
 
