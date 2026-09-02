9f9913e6309ec9b8ee30d0e3c2b48121af9a3f51
Rutvi Rajesh <rutvi.rajesh@simformsolutions.com>
2026-09-02 11:25:07 +0530

    chore: migrate notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java to aws

--- notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
+++ notification-service/src/main/java/com/piggymetrics/notification/client/AccountServiceClient.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "account-service")
+@FeignClient(
+	name = "account-service",
+	url = "${services.account-service.url:}"
+)
 public interface AccountServiceClient {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountName}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(method = RequestMethod.GET, value = "/accounts/{accountName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	String getAccount(@PathVariable("accountName") String accountName);
 
 }
