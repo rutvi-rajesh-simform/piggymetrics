6a5028a81bc3190759c2f1358265e2f92d8e8a5f
Rutvi Rajesh <rutvi.rajesh@simformsolutions.com>
2026-09-02 11:25:05 +0530

    chore: migrate account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java to aws

--- account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
+++ account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
@@ -3,13 +3,16 @@ package com.piggymetrics.account.client;
 import com.piggymetrics.account.domain.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "auth-service")
+@FeignClient(
+	name = "auth-service",
+	url = "${services.auth-service.url:http://auth-service}"
+)
 public interface AuthServiceClient {
 
-	@RequestMapping(method = RequestMethod.POST, value = "/uaa/users", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	void createUser(User user);
+	@PostMapping(value = "/uaa/users", consumes = MediaType.APPLICATION_JSON_VALUE)
+	void createUser(@RequestBody User user);
 
 }
